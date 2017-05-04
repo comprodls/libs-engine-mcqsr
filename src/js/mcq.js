12d1d2c21f010006ee91d15635d70a22cb7d3b0d
NikhilB <nikhil.batra@comprotechnologies.com>
2017-05-04 17:08:43 +0530

    #6: Update minimum required files

--- src/js/mcq.js
+++ src/js/mcq.js
@@ -1,42 +1,33 @@
 /*
- * ----------------------
- * Engine Module Renderer
- * ----------------------
+ * -------------
+ * Engine Module
+ * -------------
  * 
  * Item Type: MCQ Single Choice Quesion engine
  * Code: MCQ
- * Interface: Renderer
- *  
- * Item Render Interfaces / Modes :->
- * 
- *  1. Supports Standard ENGINE-SHELL interface
- *      {
+ * Interface: ENGINE
+ 
+ *  ENGINE Interface public functions
+ *  {
  *          init(),
  *          getStatus(),
  *          getConfig()
- *      }
+ *  }
  * 
- * ENGINE - SHELL interface : ->
  *
- * This engine loaded by another module/js "shell.js" which  establishes interface with the platform. The shell instantiates
- * this engine [ engine.init() ]  with necessary configuration paramters and a reference to platform Adapter
- * object which allows subsequent communuication with the platform.
+ * This engine is designed to be loaded dynamical by other applications (or  platforms). At the starte the function [ engine.init() ] will be called  with necessary configuration paramters and a reference to platform "Adapter"  which allows subsequent communuication with the platform.
  *
- * SHELL calls [ engine.getStatus() ] to check if SUBMIT has been pressed or not - the response from the engine is 
- * used to enable / disable LMS controls.
+ * The function [ engine.getStatus() ] may be called to check if SUBMIT has been pressed or not - the response from the engine is used to enable / disable appropriate platform controls.
  *
- * SHELL calls engine.getConfig() to request SIZE information - the response from the engine is 
- * used to resize the container iframe.
+ * The function engine.getConfig() is called to request SIZE information - the response from the engine is used to resize & display the container iframe.
  *
  *
  * EXTERNAL JS DEPENDENCIES : ->
- * Following are shared/common dependencies and assumed to loaded via the platform. The engine code can use/reference
- * these as needed
+ * Following are shared/common dependencies and assumed to loaded via the platform. The engine code can use/reference these as needed
  * 1. JQuery (2.1.1)
  * 2. Boostrap (TODO: version) 
  */
-
-define(['text!../html/mcq.html', //HTML layout(s) template (handlebars/rivets) representing the rendering UX
+define(['text!../html/mcq.html', //layout(s) template representing the UX
         'rivets',  // Rivets for data binding
         'sightglass'], //Required by Rivets
         function (mcqTemplateRef) {
@@ -46,7 +37,7 @@ define(['text!../html/mcq.html', //HTML layout(s) template (handlebars/rivets) r
     "use strict";
         
     /*
-     * Reference to platform's activity adaptor (initialized during init() ).
+     * Reference to platform's activity adaptor (passed during init() ).
      */
     var activityAdaptor;     
     
@@ -61,7 +52,7 @@ define(['text!../html/mcq.html', //HTML layout(s) template (handlebars/rivets) r
     };
     
     /*
-     * Internal Engine State.
+     * Internal Engine State - used to manage/track current status of the assessment.
      */ 
     var __state = {
         activityPariallySubmitted: false, /* State whether activity has been partially submitted. Possible Values: true/false(Boolean) */
@@ -79,54 +70,41 @@ define(['text!../html/mcq.html', //HTML layout(s) template (handlebars/rivets) r
     };
         
     /********************************************************/
-    /*                  ENGINE-SHELL INIT FUNCTION
+    /*                  INIT FUNCTION
         
         "elRoot" :->        DOM Element reference where the engine should paint itself.                                                     
-        "params" :->        Startup params passed by platform. Include the following sets of parameters:
-                        (a) State (Initial launch / Resume / Gradebook mode ).
-                        (b) TOC parameters (videoRoot, contentFile, keyframe, layout, etc.).
+        "params" :->        Startup params passed by platform. 
         "adaptor" :->        An adaptor interface for communication with platform (__saveResults, closeActivity, savePartialResults, getLastResults, etc.).
-        "htmlLayout" :->    Activity HTML layout (as defined in the TOC LINK paramter). 
-        "jsonContent" :->    Activity JSON content (as defined in the TOC LINK paramter).
-        "callback" :->      To inform the shell that init is complete.
+        "htmlLayout" :->     HTML layout  
+        "jsonContent" :->    Question JSON 
+        "callback" :->      Function to inform platform that init is complete.
     */
     /********************************************************/  
-    function init(elRoot, params, adaptor, htmlLayout, jsonContentObj, callback) {        
-
-        /* ---------------------- BEGIN OF INIT ---------------------------------*/
+    function init(elRoot, params, adaptor, htmlLayout, questionJSON, callback) {        
         //Store the adaptor  
         activityAdaptor = adaptor;
 
-        //Clone the JSON so that original is preserved.
-        var jsonContent = jQuery.extend(true, {}, jsonContentObj);
+        //Clone question JSON so that original is preserved.
+        var jsonContent = jQuery.extend(true, {}, questionJSON);
         
-        /* ------ VALIDATION BLOCK START -------- */    
-        if (jsonContent.content === undefined) {
-            if(callback) {
-                callback();
-            }       
-            //TODO - In future more advanced schema validations could be done here        
-            return; /* -- EXITING --*/
-        }
-        
-        /* ------ VALIDATION BLOCK END -------- */        
-
         /* Apply the layout HTML to the dom */
         $(elRoot).html(__constants.TEMPLATES[htmlLayout]);
 
-        /* Initialize RIVET. */
-        __initRivets(jsonContent);
+        /* Process the template by initializing RIVETs */
+         rivets.bind($('#mcq-engine'), {
+            jsonContent: jsonContent
+        });
         
-        /* Inform the shell that init is complete */
+        /* Inform the Platform that init is complete */
         if(callback) {
             callback();
         }                               
         
-        /* ---------------------- END OF INIT ---------------------------------*/
     } /* init() Ends. */        
+    
     /* ---------------------- PUBLIC FUNCTIONS --------------------------------*/
     /**
-     * ENGINE-SHELL Interface
+     * ENGINE Interface
      *
      * Return configuration
      */
@@ -135,60 +113,23 @@ define(['text!../html/mcq.html', //HTML layout(s) template (handlebars/rivets) r
     }
     
     /**
-     * ENGINE-SHELL Interface
+     * ENGINE Interface
      *
      * Return the current state (Activity Submitted/ Partial Save State.) of activity.
      */
     function getStatus() {
         return __state.activitySubmitted || __state.activityPariallySubmitted;
     }
-    
-    /**
-    * Bound to click of Activity submit button.
-    */
-    function handleSubmit(event){
-       
-       
-    }
 
-    /**
-    * Function to show user grades.
-    */
-    function showGrades(savedAnswer, reviewAttempt){
-          
-    } 
 
-    /**
-     * Function to display last result saved in LMS.
-     */ 
-    function updateLastSavedResults(lastResults) {
-        
-    }
-    /* ---------------------- PUBLIC FUNCTIONS END ----------------------------*/
-     
-
-    /* ---------------------- PRIVATE FUNCTIONS -------------------------------*/
-
-     
-    /*------------------------RIVET INITIALIZATION & BINDINGS -------------------------------*/        
-    function __initRivets(jsonContent){
-        
-        /*Bind the data to template using rivets*/
-        rivets.bind($('#mcq-engine'), {
-            jsonContent: jsonContent
-        });
-    }
-
-    /*------------------------RIVETS END-------------------------------*/
-    
     return {
-        /*Engine-Shell Interface*/
+        /*Engine Interface*/
         "init": init, /* Shell requests the engine intialized and render itself. */
         "getStatus": getStatus, /* Shell requests a gradebook status from engine, based on its current state. */
         "getConfig" : getConfig, /* Shell requests a engines config settings.  */
-        "handleSubmit" : handleSubmit,
-        "showGrades": showGrades,
-        "updateLastSavedResults": updateLastSavedResults
+        "handleSubmit" : function() {/* Do Nothing for now. Sample only*/},
+        "showGrades": function() {/* Do Nothing for now. Sample only*/},
+        "updateLastSavedResults": function() {/* Do Nothing for now. Sample only*/}
     };
     };
 });
